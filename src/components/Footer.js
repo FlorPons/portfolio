b28fd4707b8def49e0ede0071e0a045de14e5c46
FlorenciaPons <ponsmariaflorencia@gmail.com>
2025-04-05 00:35:25 -0300

    Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Footer = () => {  
     return (  
         <footer>  
+            <img className='logo' src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="Logo" />
             <h5>&copy; 2025 Florencia Pons. Todos los derechos reservados.</h5>  
         </footer>  
     );  
