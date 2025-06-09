40c71143e9df6fa1a14cffbb8188d1fe277b8325
FlorenciaPons <ponsmariaflorencia@gmail.com>
2025-06-08 21:47:20 -0300

    update

--- src/components/About.js
+++ src/components/About.js
@@ -69,8 +69,8 @@ return (
     id="about"
     initial={{ opacity: 0, y: 100 }}
     whileInView={{ opacity: 1, y: 0 }}
-    transition={{ duration: 1.2, type: "spring" }}
-    viewport={{ once: false, amount: 0.4 }}
+    transition={{ duration: 1, type: "spring" }}
+    viewport={{ once: false, amount: 0.2 }}
     className="about-section"
     >
     <div className="about-content">
